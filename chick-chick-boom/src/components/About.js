7da824aa46bf9cac2f2fbd4822139e7cd5c03b0f
tylergubser <tylergubser2200@gmail.com>
2022-07-18 17:07:50 -0500

    added info to about

--- chick-chick-boom/src/components/About.js
+++ chick-chick-boom/src/components/About.js
@@ -3,7 +3,10 @@ import React from 'react';
 function About() {
 
 return (
+    <div>
     <h1>About</h1>
+    <p>Chick Chick Boom is a one stop chicken shop. You can track your chickens progress, Upload photo's, and even get information on how to fix certain problems.</p>
+    </div>
 )
 
 }
