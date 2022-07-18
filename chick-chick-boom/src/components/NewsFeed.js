2dae03a85eabfedb2b6cb5ade90ad2b8ffa6037c
tylergubser <tylergubser2200@gmail.com>
2022-07-17 20:20:07 -0500

    added useeffect to meesages

--- chick-chick-boom/src/components/NewsFeed.js
+++ chick-chick-boom/src/components/NewsFeed.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Messages from "./Messages.js";
 
 function NewsFeed() {
     const [posts, setPosts] = useState([])
 
+    useEffect( () => {
     fetch("http://localhost:3000/posts")
     .then(resp => resp.json())
     .then(resp => setPosts(resp))
-  const post = posts.map( post => 
+    },[])
+  
+  
+    const post = posts.map( post => 
    <Messages post = {post}/> )
 
 return(
