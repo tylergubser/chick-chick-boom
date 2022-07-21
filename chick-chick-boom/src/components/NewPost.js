04876a4d0a5d66e6bc98fd65deea60409181f498
tylergubser <tylergubser2200@gmail.com>
2022-07-21 15:56:52 -0500

    update

--- chick-chick-boom/src/components/NewPost.js
+++ chick-chick-boom/src/components/NewPost.js
@@ -4,12 +4,17 @@ import React, { useState } from 'react';
 function NewPost () {
 const [comment, setComment] = useState("");
 
+
+function handleSubmit(event) {
+    
+}
 return (
 
     <div>
         <label>
             <span>Comment</span>
             <input></input>
+            <button type="button" onSubmit={handleSubmit}></button>
         </label>
         
     </div>
