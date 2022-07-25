6b5134aa7f472f8677a995603c29fdbbabd7a410
tylergubser <tylergubser2200@gmail.com>
2022-07-25 15:54:41 -0500

    update

--- chick-chick-boom/src/components/NewPost.js
+++ chick-chick-boom/src/components/NewPost.js
@@ -9,6 +9,12 @@ function handleSubmit(e) {
     e.preventDefault()
     const formData = new FormData();
         formData.append('comment', comment)
+        fetch('http://localhost:3000/posts', {
+            method: 'POST',
+            body: formData
+        })
+        .then(resp => resp.json())
+        .then(post => console.log(post))
 }
 return (
 
